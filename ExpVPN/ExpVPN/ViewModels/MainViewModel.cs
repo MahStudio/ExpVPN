e0fead6e2568cf97d4bf4b12f94560c19f2e0cb0
Mohsen Seifi <mohsens2@outlook.com>
2018-03-01 03:12:05 +0330

    Added some initial MVVM stuff

---
+++ ExpVPN/ExpVPN/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpVPN.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        // TODO : Impeliment MVVM Light
+    }
+}
